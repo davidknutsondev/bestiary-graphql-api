7f9909ffa974601120b3a96f12a920ac947e00f3
David Knutson <davidknutson@Davids-MacBook-Pro.local>
2023-09-24 16:28:40 -0400

    connect to postgres database.

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.21.0
 require (
 	github.com/graphql-go/graphql v0.8.1
 	github.com/graphql-go/handler v0.2.3
+	github.com/lib/pq v1.10.9
 )
--- main.go
+++ main.go
@@ -1,31 +1,43 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/davidknutsondev/bestiary-graphql-api/api"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+
+	dbUser := "postgres"
+	dbName := "bestiary-postgres-database"
+	dbPassword := "mysecretpassword" // Use an environment variable for the password
+
+	// Construct the connection string
+	dbInfo := fmt.Sprintf("user=%s dbname=%s password=%s sslmode=disable", dbUser, dbName, dbPassword)
+
+	// Connect to the database
+	db, err := sql.Open("postgres", dbInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// In Go, the defer statement is used to schedule a function call to be executed just before the surrounding function returns
+	// In this context it means that the db.Close() function will be called automatically when the main function exits
+	// regardless of how it exits (whether normally or due to an error)
+	defer db.Close()
+
+	// Set up HTTP handlers
 	http.Handle("/graphql", http.HandlerFunc(api.GraphQLHandler))
 	http.Handle("/sandbox", http.HandlerFunc(api.SandboxHandler))
 
-	http.ListenAndServe(":8080", nil)
+	// Start the HTTP server
+	log.Println("Server is running on :8080")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
-
-// func main() {
-// 	h := handler.New(&handler.Config{
-// 		Schema:   &schema.BeastSchema,
-// 		Pretty:   true,
-// 		GraphiQL: false,
-// 	})
-
-// 	http.Handle("/graphql", h)
-
-// 	http.Handle("/sandbox", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		w.Write(sandboxHTML)
-// 	}))
-
-// 	http.ListenAndServe(":8080", nil)
-
-// }
